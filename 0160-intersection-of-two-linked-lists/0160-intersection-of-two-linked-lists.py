a418a6b92174fcd813078e1a8ae18581eeeb917c
Atharva Ashar <102160142+AtharvaAshar@users.noreply.github.com>
2023-04-27 13:59:37 +0530

    Time: 169 ms (22.73%), Space: 31.5 MB (5.51%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-        self.visited=False
+        
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
