cf97706005cc0f5651090cb1073a3a6f1955826b
Atharva Ashar <102160142+AtharvaAshar@users.noreply.github.com>
2023-07-23 10:24:11 +0530

    Time: 45 ms (61.82%), Space: 16.4 MB (31.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n==0: return 0
-        if n==1: return n
-        if n==2: return 2
+        elif n==1: return n
+        elif n==2: return 2
         oneStepback=2
         twoStepback=1
         ans=0
