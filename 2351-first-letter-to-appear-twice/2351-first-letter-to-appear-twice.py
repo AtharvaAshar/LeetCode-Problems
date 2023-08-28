63b55f836564ba2502b6ef03392f126582f683e9
Atharva Ashar <102160142+AtharvaAshar@users.noreply.github.com>
2023-08-28 22:52:59 +0530

    Time: 37 ms (76.87%), Space: 16.2 MB (87.64%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -5,8 +5,7 @@ class Solution:
             if s[i] not in d:
                 d[s[i]]=1
             else:
-                d[s[i]]+=1
-            
-            if d[s[i]]==2:
                 return s[i]
+            
+            
         
